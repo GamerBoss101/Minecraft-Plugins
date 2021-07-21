c5163b4d5c60aa0b87e3ef4d6041df9c0280481f
GamerBoss101 <adithcool.5@gmail.com>
2021-07-21 18:45:04 -0400

    all code

---
+++ Mob-Kill-Effects/src/org/bosstop/mobkilleffects/Main.java
@@ -0,0 +1,29 @@
+package org.bosstop.mobkilleffects;
+
+import org.bosstop.mobkilleffects.commands.BaseCommands;
+import org.bosstop.mobkilleffects.mobkill.KillListener;
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+	
+	@Override
+	public void onEnable(){
+		
+		saveDefaultConfig();
+		
+		String prefix = "&f[&3MobKillEffects&f] ";
+		
+		System.out.println(ChatColor.translateAlternateColorCodes('&', prefix + "&aStarting Mob Kill Effects v1.4\n"
+				+ prefix + "&aMob Kill Effects Active"
+				));
+		
+		new BaseCommands(this);
+		
+		new KillListener(this);
+		
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+}
---
+++ Mob-Kill-Effects/src/org/bosstop/mobkilleffects/mobkill/KillListener.java
@@ -0,0 +1,47 @@
+package org.bosstop.mobkilleffects.mobkill;
+
+import org.bosstop.mobkilleffects.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+import io.netty.util.internal.ThreadLocalRandom;
+
+public class KillListener implements Listener {
+	
+	private Main plugin;
+	
+	public KillListener(Main plugin) {
+		this.plugin = plugin;
+		
+		Bukkit.getPluginManager().registerEvents(this, plugin);
+	}
+
+	@EventHandler
+	public void onKill(EntityDeathEvent e) {
+	    LivingEntity entity = e.getEntity();
+	    Player killer = entity.getKiller();
+	    
+	    int rnd = ThreadLocalRandom.current().nextInt(PotionEffectType.values().length);
+	    
+	    String time = plugin.getConfig().getString("potionEffect_time");
+	    
+		 int i = Integer.parseInt(time);
+		 
+	    if (killer != null) {
+	    	
+			killer.addPotionEffect(new PotionEffect(PotionEffectType.values()[rnd], i, 1));
+			
+			killer.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cYou have gotten a Random Effect"));
+			
+	    } else {
+	    	return;
+	    }
+	}
+}
